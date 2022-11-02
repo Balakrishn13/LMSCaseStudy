18adf20ec2d9dae7f97b7cde635364f194c010cf
cogdotnet1631 <cogdotnet1631@CTSDOTNET136>
2022-11-02 12:55:37 +0530

    WithAzure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  companybaseUrl: "https://lmsadminapi.azurewebsites.net/api/v1.0/lms/company/",
+  coursesbaseUrl: "https://lmsuserapi.azurewebsites.net/api/v1.0/lms/courses/",
+  baseUrl:"https://lmsadminapi.azurewebsites.net/api/v1.0/lms/authenticate"
 };
